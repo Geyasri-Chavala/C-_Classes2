dc81309c77e435370f03cc6ff30516a017888ac6
Geyasri-Chavala <geyasri.fullstack@gmail.com>
2025-11-23 23:29:18 +0530

    Implement&Abstract

---
+++ C#_Classes2/11.Implementation/RemoteControlSystem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RemoteSystem
+{
+    // Basic remote functions
+    interface IRemote
+    {
+        void PowerOn();
+        void PowerOff();
+        void IncreaseVolume();
+        void ChangeChannel(int channelNumber);
+    }
+
+    // Extra feature: Price
+    interface IRemotePrice
+    {
+        string GetPrice();
+    }
+
+    // Extra feature: Location
+    interface IRemoteLocation
+    {
+        string GetLocation();
+    }
+
+    // Sony implements IRemote + IRemotePrice
+    class SonyRemote : IRemote, IRemotePrice
+    {
+        public void PowerOn() => Console.WriteLine("Sony TV ON");
+        public void PowerOff() => Console.WriteLine("Sony TV OFF");
+        public void IncreaseVolume() => Console.WriteLine("Sony Volume Increased");
+        public void ChangeChannel(int ch) => Console.WriteLine($"Sony changed to channel {ch}");
+        public string GetPrice() => "Price: 500 Rupees";
+    }
+
+    // LG implements IRemote + IRemoteLocation
+    class LGRemote : IRemote, IRemoteLocation
+    {
+        public void PowerOn() => Console.WriteLine("LG TV ON");
+        public void PowerOff() => Console.WriteLine("LG TV OFF");
+        public void IncreaseVolume() => Console.WriteLine("LG Volume Increased");
+        public void ChangeChannel(int ch) => Console.WriteLine($"LG changed to channel {ch}");
+        public string GetLocation() => "Location: Bangalore";
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            IRemote sony = new SonyRemote();
+            sony.PowerOn();
+            sony.ChangeChannel(5);
+
+            IRemotePrice sonyPrice = new SonyRemote();
+            Console.WriteLine(sonyPrice.GetPrice());
+
+            IRemote lg = new LGRemote();
+            lg.PowerOn();
+            lg.ChangeChannel(10);
+
+            IRemoteLocation lgLocation = new LGRemote();
+            Console.WriteLine(lgLocation.GetLocation());
+        }
+    }
+}
---
+++ C#_Classes2/12.Abstraction/HomeAppliancesystem.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace HomeApplianceSystem
+{
+    // Abstract parent class
+    abstract class Appliance
+    {
+        public string Brand { get; set; }
+        public int PowerUsageWatts { get; set; }
+
+        // Abstract methods (rules)
+        public abstract void TurnOn();
+        public abstract void TurnOff();
+        public abstract void ShowMode();
+
+        // Concrete method
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Power Usage: {PowerUsageWatts}W");
+        }
+    }
+
+    // Child class 1
+    class WashingMachine : Appliance
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Washing Machine is now ON.");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Washing Machine is now OFF.");
+        }
+
+        public override void ShowMode()
+        {
+            Console.WriteLine("Current Mode: Quick Wash.");
+        }
+    }
+
+    // Child class 2
+    class Refrigerator : Appliance
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Refrigerator is now cooling.");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Refrigerator is turned OFF.");
+        }
+
+        public override void ShowMode()
+        {
+            Console.WriteLine("Cooling Mode: Eco Mode.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Appliance wm = new WashingMachine()
+            {
+                Brand = "LG",
+                PowerUsageWatts = 1500
+            };
+
+            wm.TurnOn();
+            wm.ShowMode();
+            wm.ShowDetails();
+            wm.TurnOff();
+
+            Console.WriteLine();
+
+            Appliance fridge = new Refrigerator()
+            {
+                Brand = "Samsung",
+                PowerUsageWatts = 200
+            };
+
+            fridge.TurnOn();
+            fridge.ShowMode();
+            fridge.ShowDetails();
+            fridge.TurnOff();
+        }
+    }
+}
